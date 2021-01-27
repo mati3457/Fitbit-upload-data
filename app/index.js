a158a10fc581a9fa8b23997c5dcf8962153f01f3
mati3457 <mati3457@gmail.com>
2021-01-27 13:43:18 +0100

    Multiple changes fix display staying on while sleep, fix filename creating add UI feature add new icnos refactor all functions

--- app/index.js
+++ app/index.js
@@ -1,16 +1,51 @@
+// ideas to upgrade app:
+// getting batch data from sensors, example: https://dev.fitbit.com/build/guides/sensors/accelerometer/
+
 import document from "document";
 import { HeartRateSensor } from "heart-rate";
 import { Accelerometer } from "accelerometer";
 import { outbox } from "file-transfer"
+import { me } from "appbit";
+import clock from "clock";
 import * as fs from "fs";
 
-const fileName = "/private/data/ascii.txt";
+const timeZone = 1;
+const FILE_PATH = "/private/data/";
+const RECORD_ICON_PATH = "icons/icon-record.png";
+const STOP_RECORD_ICON_PATH = "icons/icon-stop-record.png";
+const RECORD_BTN_VALUE = "Record";
+const STOP_RECORD_BTN_VALUE = "Stop";
+let fileName;
+let resultString = "";
+let refreshIntervalId;
+
+// sampling at 1Hz (once per second)
+const hrm = new HeartRateSensor({ frequency: 1 });
+const accel = new Accelerometer({ frequency: 1 });
+
+
+// app does not time out after period of inactivity
+me.appTimeoutEnabled = false;
+
 // data on screen
+let timeLabel = document.getElementById("timeLabel");
+let errorLabel = document.getElementById("errorLabel");
+// manage button listener
+let recordButton = document.getElementById("btn-record");
 
-const appStatusText = document.getElementById("appStatusLabel");
-appStatusText.text = "ON";
-
-const timeLabel = document.getElementById("timeLabel");
+recordButton.onactivate = function(evt) {
+  if (recordButton.text == RECORD_BTN_VALUE) {
+    recordButton.text = STOP_RECORD_BTN_VALUE;
+    recordButton.image = STOP_RECORD_ICON_PATH
+    startSensors();
+    refreshIntervalId = setInterval(recordData, 1000);
+  } else {
+    recordButton.text = RECORD_BTN_VALUE;
+    recordButton.image = RECORD_ICON_PATH
+    stopSensors();
+    clearInterval(refreshIntervalId);
+  }
+}
 
 const updateClock = (h, m, s) => {
   h = addTimeZone(h);
@@ -19,26 +54,42 @@ const updateClock = (h, m, s) => {
   timeLabel.text = h + ":" + m + ":" + s;
 }
 
-// import { memory } from "system";
+const startSensors = () => {
+  hrm.start();
+  accel.start();
+}
 
-import { me } from "appbit";
-import clock from "clock";
+const stopSensors = () => {
+  hrm.stop();
+  accel.stop();
+}
 
-import { display } from "display";
+const sendFile = (payload) => {
+  fileName = FILE_PATH + makeFileName();
+  fs.writeFileSync(fileName, payload, "ascii");
+  outbox
+    .enqueueFile(fileName)
+    .then(ft => {
+      console.log(`Transfer of ${ft.name} successfully queued.`);
+      fs.unlinkSync(fileName);
+    })
+    .catch(err => {
+      errorLabel.text = "FILE SEND ERROR";
+      console.log(`Failed to schedule transfer: ${err}`);
+      fs.unlinkSync(fileName);
+    })
+}
 
-// app doesnt time out after period of inactivity
-me.appTimeoutEnabled = false;
-
-
-
-const timeZone = 1; // set time zone value
-
-// pokazywanie czasu
+const makeFileName = () => {
+  let date = new Date(new Date().getTime() - 5 * 60000 + timeZone * 60 * 60000);
+  let dateF = date.toLocaleDateString("en-CA");
+  let timeF = date.toLocaleTimeString("pl-PL").replace(/:/g, '-').slice(0, -7);
+  return dateF + "_" + timeF;
+}
+ 
 const round2decimals = (num) => {
   return Math.round(num * 100) / 100;
 }
-
-
 
 const addTimeZone = (hour) => {
   if(hour == "23") {
@@ -47,57 +98,39 @@ const addTimeZone = (hour) => {
   return (Number(hour) + timeZone).toString();
 }
 
-let timeString = "";
-let accelString = "";
+const addMeasureData = (data, time) => {
+  let fResult = data;
+  let accelString = round2decimals(accel.x) + "," + round2decimals(accel.y) + "," + round2decimals(accel.z);
+  fResult += 
+    time.getHours() + 
+    ":" + time.getMinutes() + 
+    ":" + time.getSeconds() + 
+    "," + ((hrm.heartRate == null) ? "00" : hrm.heartRate) +
+     "," + accelString + "\n";
+  return fResult;
+}
 
-display.autoOff = false;
-display.brightnessOverride = "dim";
-
-if (fs.existsSync(fileName)) {
-  console.log("file exists!");
-  if(fs.readFileSync("ascii.txt", "ascii") == "") {
-    console.log("File contains something -- removing file...");
-    fs.unlinkSync(fileName);
-    fs.writeFileSync(fileName, "", "ascii");
+const recordData = () => {
+  let time = new Date();
+  resultString = addMeasureData(resultString, time);
+  if (time.getMinutes() % 5 == 0 && time.getSeconds() == 0) {
+    sendFile(resultString);
+    resultString = "";
   }
-} else {
-  fs.writeFileSync(fileName, "", "ascii");
-}
-
-if(HeartRateSensor) {
-  var hrm = new HeartRateSensor({ frequency: 1 });
-  hrm.start();
-}
-
-if (Accelerometer) {
-  // sampling at 1Hz (once per second)
-  const accel = new Accelerometer({ frequency: 1 });
-  accel.start();
-}
-
-clock.granularity = "seconds";
-clock.ontick = (evt) => {
-  let [hour, minute, second] = evt.date.toTimeString().split(':')
-  accelString = round2decimals(accel.x) + "," + round2decimals(accel.y) + "," + round2decimals(accel.z);
-  timeString += addTimeZone(hour) + ":" + minute + ":" + Math.floor(second) + "," + ((hrm.heartRate == null) ? "00" : hrm.heartRate) + "," + accelString + "\n";
-
-  // console.log("JS memory: " + memory.js.used + "/" + memory.js.total);
-  updateClock(hour, minute, second);
-
-  if(minute % 5 == 0 && second == 0) {
-    fs.writeFileSync(fileName, timeString, "ascii");
-    timeString = "";
-    outbox
-    .enqueueFile(fileName)
-    .then(ft => {
-      console.log(`Transfer of ${ft.name} successfully queued.`);
-      fs.unlinkSync(fileName);
-      // if(fs.existsSync("/private/data/ascii.txt")) {
-      //   console.log("Plik istnieje")
-      // }
-    })
-    .catch(err => {
-      console.log(`Failed to schedule transfer: ${err}`);
-    })
+  if (time.getMinutes() % 5 == 0 && time.getSeconds() == 1) {
+    if (resultString.length > 1000) {
+      sendFile(resultString);
+      resultString = "";
+    }
   }
 }
+
+const startClockTick = () => {
+  clock.granularity = "seconds";
+  clock.ontick = (evt) => {
+    let [hour, minute, second] = evt.date.toTimeString().split(':')
+      updateClock(hour, minute, second);
+  }
+}
+
+startClockTick();
--- companion/index.js
+++ companion/index.js
@@ -1,57 +1,46 @@
 import { inbox } from "file-transfer";
-import * as fs from "fs";
 
-let srcStorage = "http://192.168.1.19/file-storage-local/index.php";
-console.log("im here");
+// set POST target here
+const POST_PATH = "http://192.168.1.19/file-storage-local/index.php";
+console.log("Companion response.");
 let queueFiles = [];
 
 const upload = (fileContents, fileName) => {
-  // console.log(fileName);
   let flag = true;
   let newFile = new File([fileContents], fileName);
   const formData = new FormData();
   formData.append('file', newFile);
-
-  fetch(srcStorage, { // Your POST endpoint
+  fetch(POST_PATH, { // POST endpoint
     method: 'POST',
-    body: formData // This is your file object
+    body: formData // file object
   })
   .then(
     success => { // Handle the success response object
-      console.log("Succ" + success)
+      console.log("Success, file sent to POST endpoint")
       flag = true;
      } 
   ).catch(
     error => { // Handle the error response object
-      console.log("Err" + error)
+      console.log("Error: " + error)
       flag = false;
     }
   );
   return flag;
 };
 
-const now = () => {
-  let actDate = new Date();
-  let dateString = actDate.getFullYear() + "-" + (actDate.getMonth()+1) + "-" + actDate.getDate() + "_" + actDate.getHours() + "-" + actDate.getMinutes();
-  if(actDate.getMinutes() == 0) {
-    dateString += "0";
-  }
-  return dateString;
-}
-
 async function processAllFiles() {
     let file;
     while ((file = await inbox.pop())) {
       const payload = await file.text();
-      let timeNow = new Date()
-      let fileName = now();
+      //let fileName = now();
+      let fileName = file.name;
       let status = upload(payload, fileName);
-      
+
       if (!status) {
         if (queueFiles.length > 10) queueFiles.shift();
-        queueFiles.push([payload, fileName]);
+          queueFiles.push([payload, fileName]);
       } else {
-        while (queueFiles != [] || !status) {
+        while (queueFiles.length && status) {
           let [value, name] = queueFiles.shift();
           status = upload(value, name);
         }
@@ -59,9 +48,9 @@ async function processAllFiles() {
     }
 }
   
-  inbox.addEventListener("newfile", processAllFiles);
-  
-  processAllFiles()
+inbox.addEventListener("newfile", processAllFiles);
+
+processAllFiles()
 
 
   
