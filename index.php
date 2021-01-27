e58a878fa91955c2f961f98d12b8109e12ea9be8
Mateusz Bienia <mati3457@gmail.com>
2021-01-27 13:55:05 +0100

    Create folder structure while saving file

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
-    $file = $_FILES['file'];
-    $date = date('Y-m-d_H-i');//, strtotime('-1 hour'));
-    rename($file['tmp_name'], $file['name']);
-    // zrobic tak zeby nazywać po ostatniej wartosci w pliku czy cos :) albo po czasie dodania do kolejki
+    if(!empty($_FILES['file'])) {
+        $file = $_FILES['file'];
+        $date = date('Y-m-d_H-i');//, strtotime('-1 hour'));
+        rename($file['tmp_name'], $file['name']);
+        $month = date('F');
+        $day = date('j');
+        if (!is_dir("Data/$month")) {
+            mkdir("Data/$month", 0777);
+        }
+        if (!is_dir("Data/$month/$day")) {
+            mkdir("Data/$month/$day", 0777);
+        }
+        $destdir = "Data/$month/$day";
+
+        $data=file_get_contents($file['name']);
+        echo $data;
+        $file_name = $file['name'];
+        file_put_contents($destdir."/".substr( $file_name,strrpos( $file_name,'/')), $data);
+        unlink($file['name']);
+    }
 ?>
